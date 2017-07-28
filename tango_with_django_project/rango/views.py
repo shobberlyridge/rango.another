fc37c5516727e822237015af13d68c66f250193f
Edward Parker <edward@albany.me.uk>
2017-07-28 19:13:56 +0100

    p27

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-	return HttpResponse("Rango says Hello there Partner!")
+	return HttpResponse("Rango says Hello there Partner! <br/> <a href='/rango/about/'>About</a>")
 
+def about(request):
+	return HttpResponse("Rango says here is the About page. <br/> <a href='/rango/'>Index</a>")
 
