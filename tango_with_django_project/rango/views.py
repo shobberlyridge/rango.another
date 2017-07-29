c178726358c0ef6a5e18eafbf78225bef0053df5
Edward Parker <edward@albany.me.uk>
2017-07-29 17:26:12 +0100

    first commit from artemis

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-	return HttpResponse("Rango says Hello there Partner! <br/> <a href='/rango/about/'>About</a>")
+	return HttpResponse("Rango says Hello there Partner! - from artemis, no less <br/> <a href='/rango/about/'>About</a>")
 
 def about(request):
 	return HttpResponse("Rango says here is the About page. <br/> <a href='/rango/'>Index</a>")
