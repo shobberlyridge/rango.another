66eb348c4743f1c896039854d41c9d0810d28968
Edward Parker <edward@albany.me.uk>
2017-07-30 12:35:55 +0100

    4.1 -> 4.2

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -2,7 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-	return HttpResponse("Rango says Hello there Partner! <br/> <a href='/rango/about/'>About</a>")
+	# Construct dictionary to pass to template
+	context_dict = {'boldmessage': "Crunchy................"}
+	
+	# Return rendered response
+	# Note first parameter is the template to use
+	return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
 	return HttpResponse("Rango says here is the About page. <br/> <a href='/rango/'>Index</a>")
