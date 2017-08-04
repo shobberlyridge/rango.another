5ba3806ac4512b943e1a934a1a7f8966b44ea644
Edward Parker <edward@albany.me.uk>
2017-08-04 11:29:49 +0100

    down the stairs

--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
     views = models.IntegerField(default=0)
--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rango.models import Category
 
 def index(request):
-	# Construct dictionary to pass to template
-	context_dict = {'boldmessage': "Crunchy................"}
-	
-	# Return rendered response
-	# Note first parameter is the template to use
-	return render(request, 'rango/index.html', context=context_dict)
+    # Query the database for a list of ALL categories currently stored.
+    # Order the categories by no. likes in descending order.
+    # Retrieve the top 5 only - or all if less than 5.
+	# Place the list in our context_dict dictionary
+    # that will be passed to the template engine.
+	category_list = Category.objects.order_by('-likes')[:5]
+	context_dict = {'categories': category_list}
+	# Render the response and send it back!
+	return render(request, 'rango/index.html', context_dict)
 
 def about(request):
 	context_dict = {'aboutmessage': "Rango says here is the About page."}
