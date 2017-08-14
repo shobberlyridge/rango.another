5f28fea188004b47615ee9c09b48d8af1f35510b
Edward Parker <edward@albany.me.uk>
2017-08-14 17:14:58 +0100

    --> 7

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -12,6 +12,10 @@ def index(request):
 	category_list = Category.objects.order_by('-likes')[:5]
 	context_dict = {'categories': category_list}
 	# Render the response and send it back!
+	
+	page_list = Page.objects.order_by('-views')[:5]
+	context_dict['pages'] = page_list
+	
 	return render(request, 'rango/index.html', context_dict)
 	
 def show_category(request, category_name_slug):
